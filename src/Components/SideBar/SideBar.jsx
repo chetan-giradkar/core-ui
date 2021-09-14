1b3ec564aad80dce0ef337f0b3759720c49aa217
Chetan Giradkar <giradkarchetan@gmail.com>
2021-09-15 02:23:06 +0530

    Add SideBar code

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { FaChevronLeft } from "react-icons/fa";
 
 const SideBar = () => {
+    const [open, setopen] = useState(false);
+
     return (
         <div className="pages sidebar">
-            SideBar
+            <div className={open ? "sidebar-section sidebar-open" : "sidebar-section sidebar-close"}>
+                SideBar Content
+            </div>
+            <div className="arrow-section" onClick={()=>setopen(!open)}>
+                <FaChevronLeft className={open ? "sidebar-chevron chevron-open" : "sidebar-chevron chevron-close"} />
+            </div>
         </div>
-    )
+    );
 }
 
-export default SideBar
+export default SideBar;
